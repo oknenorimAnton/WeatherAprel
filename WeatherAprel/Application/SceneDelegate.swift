d82b6cd732c917fe1fbd01a40130445044752ad8
Anton Mironenko <103453526+oknenorimAnton@users.noreply.github.com>
2023-05-02 20:37:41 +0300

    Первоначальная настройка проекта

---
+++ WeatherAprel/Application/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  WeatherAprel
+//
+//  Created by Антон on 02.05.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let rootViewController = ViewController()
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+
+
+}
+
